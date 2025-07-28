62975f3c6a1519585957d60faa299ee31aff59ea
Divyanshu <lyricalv15@gmail.com>
2025-07-28 23:21:54 +0530

    Stock-Buy-And-Sell

---
+++ GFG 160/Arrays/StockBuyAndSell.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def maximumProfit(self, prices) -> int:
+        # code here
+        profit = 0
+        # Profit = Sell price - cost price
+        
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+                
+        return profit
+        
